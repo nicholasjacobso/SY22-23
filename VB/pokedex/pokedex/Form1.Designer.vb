a14894a67203585f6085dd34e40154ab9bdbc1dc
nicholas <njac145@learner.dvusd.org>
2022-08-18 13:52:57 -0700

    changed on thursday

---
+++ VB/pokedex/pokedex/Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.namelabel = New System.Windows.Forms.Label()
+        Me.legslabel = New System.Windows.Forms.Label()
+        Me.hightlabel = New System.Windows.Forms.Label()
+        Me.candylabel = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.mewtwoPictureBox = New System.Windows.Forms.PictureBox()
+        Me.charmanderPictureBox = New System.Windows.Forms.PictureBox()
+        Me.bulbasourPictureBox = New System.Windows.Forms.PictureBox()
+        Me.squrtilePictureBox = New System.Windows.Forms.PictureBox()
+        Me.pikachuePictureBox = New System.Windows.Forms.PictureBox()
+        Me.displayPictureBox = New System.Windows.Forms.PictureBox()
+        CType(Me.mewtwoPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.charmanderPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.bulbasourPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.squrtilePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pikachuePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(83, 228)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "pikachue"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(466, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(474, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 16)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "legs"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(475, 102)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "hight"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(440, 135)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "favorite candy"
+        '
+        'namelabel
+        '
+        Me.namelabel.AutoSize = True
+        Me.namelabel.Location = New System.Drawing.Point(535, 28)
+        Me.namelabel.Name = "namelabel"
+        Me.namelabel.Size = New System.Drawing.Size(10, 16)
+        Me.namelabel.TabIndex = 7
+        Me.namelabel.Text = " "
+        '
+        'legslabel
+        '
+        Me.legslabel.AutoSize = True
+        Me.legslabel.Location = New System.Drawing.Point(535, 59)
+        Me.legslabel.Name = "legslabel"
+        Me.legslabel.Size = New System.Drawing.Size(10, 16)
+        Me.legslabel.TabIndex = 8
+        Me.legslabel.Text = " "
+        '
+        'hightlabel
+        '
+        Me.hightlabel.AutoSize = True
+        Me.hightlabel.Location = New System.Drawing.Point(535, 102)
+        Me.hightlabel.Name = "hightlabel"
+        Me.hightlabel.Size = New System.Drawing.Size(10, 16)
+        Me.hightlabel.TabIndex = 9
+        Me.hightlabel.Text = " "
+        '
+        'candylabel
+        '
+        Me.candylabel.AutoSize = True
+        Me.candylabel.Location = New System.Drawing.Point(561, 135)
+        Me.candylabel.Name = "candylabel"
+        Me.candylabel.Size = New System.Drawing.Size(10, 16)
+        Me.candylabel.TabIndex = 10
+        Me.candylabel.Text = " "
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(210, 228)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "squrtile"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(333, 228)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 13
+        Me.Button3.Text = "bulbasour"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(443, 228)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 17
+        Me.Button4.Text = "charmander"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(564, 228)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 18
+        Me.Button5.Text = "mewtwo"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'mewtwoPictureBox
+        '
+        Me.mewtwoPictureBox.Image = Global.pokedex.My.Resources.Resources.download_mewtwo_2_
+        Me.mewtwoPictureBox.Location = New System.Drawing.Point(548, 261)
+        Me.mewtwoPictureBox.Name = "mewtwoPictureBox"
+        Me.mewtwoPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.mewtwoPictureBox.TabIndex = 19
+        Me.mewtwoPictureBox.TabStop = False
+        '
+        'charmanderPictureBox
+        '
+        Me.charmanderPictureBox.Image = Global.pokedex.My.Resources.Resources.download_charmander__1_
+        Me.charmanderPictureBox.Location = New System.Drawing.Point(431, 261)
+        Me.charmanderPictureBox.Name = "charmanderPictureBox"
+        Me.charmanderPictureBox.Size = New System.Drawing.Size(100, 47)
+        Me.charmanderPictureBox.TabIndex = 16
+        Me.charmanderPictureBox.TabStop = False
+        '
+        'bulbasourPictureBox
+        '
+        Me.bulbasourPictureBox.Image = Global.pokedex.My.Resources.Resources.download_bulb
+        Me.bulbasourPictureBox.Location = New System.Drawing.Point(322, 261)
+        Me.bulbasourPictureBox.Name = "bulbasourPictureBox"
+        Me.bulbasourPictureBox.Size = New System.Drawing.Size(100, 46)
+        Me.bulbasourPictureBox.TabIndex = 14
+        Me.bulbasourPictureBox.TabStop = False
+        '
+        'squrtilePictureBox
+        '
+        Me.squrtilePictureBox.Image = Global.pokedex.My.Resources.Resources._007
+        Me.squrtilePictureBox.Location = New System.Drawing.Point(199, 261)
+        Me.squrtilePictureBox.Name = "squrtilePictureBox"
+        Me.squrtilePictureBox.Size = New System.Drawing.Size(100, 46)
+        Me.squrtilePictureBox.TabIndex = 12
+        Me.squrtilePictureBox.TabStop = False
+        '
+        'pikachuePictureBox
+        '
+        Me.pikachuePictureBox.Image = Global.pokedex.My.Resources.Resources.picachu
+        Me.pikachuePictureBox.Location = New System.Drawing.Point(69, 257)
+        Me.pikachuePictureBox.Name = "pikachuePictureBox"
+        Me.pikachuePictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.pikachuePictureBox.TabIndex = 2
+        Me.pikachuePictureBox.TabStop = False
+        '
+        'displayPictureBox
+        '
+        Me.displayPictureBox.Location = New System.Drawing.Point(-1, -1)
+        Me.displayPictureBox.Name = "displayPictureBox"
+        Me.displayPictureBox.Size = New System.Drawing.Size(450, 223)
+        Me.displayPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.displayPictureBox.TabIndex = 1
+        Me.displayPictureBox.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.mewtwoPictureBox)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.charmanderPictureBox)
+        Me.Controls.Add(Me.bulbasourPictureBox)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.squrtilePictureBox)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.candylabel)
+        Me.Controls.Add(Me.hightlabel)
+        Me.Controls.Add(Me.legslabel)
+        Me.Controls.Add(Me.namelabel)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pikachuePictureBox)
+        Me.Controls.Add(Me.displayPictureBox)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.mewtwoPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.charmanderPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.bulbasourPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.squrtilePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pikachuePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.displayPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents displayPictureBox As PictureBox
+    Friend WithEvents pikachuePictureBox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents namelabel As Label
+    Friend WithEvents legslabel As Label
+    Friend WithEvents hightlabel As Label
+    Friend WithEvents candylabel As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents squrtilePictureBox As PictureBox
+    Friend WithEvents bulbasourPictureBox As PictureBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents charmanderPictureBox As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents mewtwoPictureBox As PictureBox
+End Class
---
+++ VB/pokedex/pokedex/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles displayPictureBox.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        displayPictureBox.Image = pikachuePictureBox.Image
+        namelabel.Text = "pikachue"
+        legslabel.Text = "4"
+        hightlabel.Text = "1 foot"
+        candylabel.Text = "ketchup"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        displayPictureBox.Image = squrtilePictureBox.Image
+        namelabel.Text = "squirtle"
+        legslabel.Text = "2"
+        hightlabel.Text = "1 foot"
+        candylabel.Text = "acorns"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        displayPictureBox.Image = bulbasourPictureBox.Image
+        namelabel.Text = "bulbasaur"
+        legslabel.Text = "4"
+        hightlabel.Text = "2 foot"
+        candylabel.Text = "sunlight"
+
+    End Sub
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+        displayPictureBox.Image = charmanderPictureBox.Image
+        namelabel.Text = "charmander"
+        legslabel.Text = "2"
+        hightlabel.Text = "2 foot"
+        candylabel.Text = "apples"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        displayPictureBox.Image = mewtwoPictureBox.Image
+        namelabel.Text = "mewtwo"
+        legslabel.Text = "2"
+        hightlabel.Text = "6 foot"
+        candylabel.Text = "oran berry"
+    End Sub
+End Class
